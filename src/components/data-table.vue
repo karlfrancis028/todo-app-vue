8460e1b92d5a7e3a73b4b1d13c6f8893582ce764
Full Name <kflinget@gmail.com>
2023-04-28 02:07:12 +0800

    Added data-table component

---
+++ src/components/data-table.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="data-table">
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "data-table",
+  }
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import OneColLayout from './one-col-layout';
+import DataTable from './data-table';
 
 export default {
   OneColLayout,
+  DataTable
 }
