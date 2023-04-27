4c83eb666ff3c2a471f79f62b99dacb392b6efbd
Full Name <kflinget@gmail.com>
2023-04-27 22:14:31 +0800

    Created reusable one-col-layout

---
+++ src/components/one-col-layout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="one-col-layout">
+    <header class="one-col-layout__header">
+      <slot name="header"></slot>
+    </header>
+    <main class="one-col-layout__main">
+      <slot name="content"></slot>
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "one-col-layout",
+  }
+</script>
+
+<style scoped lang="scss">
+  .one-col-layout {
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 20px;
+
+    &__header {
+      margin-bottom: 20px;
+    }
+  }
+</style>
