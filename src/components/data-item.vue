c2dfce06fd3d6950755d3c00bef82dc1107329f6
Full Name <kflinget@gmail.com>
2023-04-28 02:16:36 +0800

    Added data-item component

---
+++ src/components/data-item.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="data-item">
+    {{ data }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "data-item",
+    props: {
+      data: {
+        type: Object,
+        default: () => {}
+      },
+    },
+  }
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import OneColLayout from './one-col-layout';
 import DataTable from './data-table';
+import DataItem from './data-item';
 
 export default {
   OneColLayout,
-  DataTable
+  DataTable,
+  DataItem,
 }
